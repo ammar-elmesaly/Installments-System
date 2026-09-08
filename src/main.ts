06dfffdad53dea62ac34c168af56ff3c2aeaba21
ammar-elmesaly <amarmazn4@gmail.com>
2026-09-08 09:11:59 +0300

    b

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
   app.setGlobalPrefix('api');
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Automatically strip non-whitelisted properties
-    transform: true, // Automatically transform payloads to match DTO types
-  }));
-  
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
