4906c9cbb0d79dc3df5db234ab9287ab200ffa93
ammar-elmesaly <amarmazn4@gmail.com>
2026-09-09 02:56:40 +0300

    Fix main.js file not found

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-  await app.init();
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Automatically strip non-whitelisted properties
+    transform: true, // Automatically transform payloads to match DTO types
+  }));
+  
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
-
-export default server;
